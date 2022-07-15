d7ee2c3bf6f7de86f10ee7549e24a42fdf61e2db
hien.vo <hien.vo@PCX-HienVo.local>
2022-07-15 17:34:36 +0700

    render movies

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <h1> NETFLIX  </h1>
         <Row title="NETFLIX ORIGINAL" fetchURL={requests.fetchNetflixOriginals}/>
         <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+        <Row title="Top Rate" fetchURL={requests.fetchTopRate}/>
+        <Row title="Action movies" fetchURL={requests.fetchActionMovies}/>
+        <Row title="Comedy movies" fetchURL={requests.fetchComedyMovies}/>
+        <Row title="Horror movies" fetchURL={requests.fetchHorrorMovies}/>
+        <Row title="Romance movies" fetchURL={requests.fetchRomanceMovies}/>
+        <Row title="Documentation movies" fetchURL={requests.fetchDocumentariesMovies}/>
     </div>
   );
 }
--- src/component/Row.js
+++ src/component/Row.js
@@ -1,12 +1,39 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from './axios';
+import row from '../style/row.css';
+
+const base_url = 'http://image.tmdb.org/t/p/original/';
 
 function Row( {title, fetchURL} ) {
 
-    const [moives, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const requests = await axios.get(fetchURL);
+
+      //https://api.themoviedb.org/3/discover/tv?api_key=3d028677c99adb2a0492fff4113ec0bb&with_networks=213
+      
+      setMovies(requests.data.results);
+      return requests;
+      console.log(requests);
+    }
+    fetchData();
+  },[fetchURL]);
+
+  console.table(movies);
 
   return (
-    <div>
+    <div className='row'>
+
         <h2>{title}</h2>
+        <div className='row__posters'>
+            {movies.map((movie) => (
+              <img className='row__poster' src={`${base_url}${movie?.poster_path}`} alt={movie.name}/> 
+            ))}
+        </div>
+
+        
     </div>
   )
 }
